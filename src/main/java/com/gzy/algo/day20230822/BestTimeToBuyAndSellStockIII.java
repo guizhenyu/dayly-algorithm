9a2497be1a615d7a0f416966193fb76b35b55ed9
guizhenyu <guizy@deepblueai.com>
2023-08-22 09:55:06 +0800

    add BestTimeToBuyAndSellStock

---
+++ src/main/java/com/gzy/algo/day20230822/BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,24 @@
+package com.gzy.algo.day20230822;
+
+public class BestTimeToBuyAndSellStockIII {
+
+
+    /**
+     * You are given an array prices where prices[i] is the price of a given stock on the i-th day.
+     * Find the maximum profit you can achieve. You may complete at most two transactions.
+     *
+     * Note:
+     * You may not engage int multiple transactions simultaneously (i.e, you must sell the stock before you buy again).
+     *
+     * Constraints:
+     * 1 <= prices.length <= 10^5
+     * 0 <= prices[i] <= 10^5
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices){
+        
+
+        return 0;
+    }
+}
