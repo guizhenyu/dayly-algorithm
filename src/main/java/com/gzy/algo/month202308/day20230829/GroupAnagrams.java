de44fd37cbcf14ac562806ad51f6ec0026ff7d19
guizhenyu <guizy@deepblueai.com>
2023-08-30 18:28:30 +0800

    Add GroupAnagrams

---
+++ src/main/java/com/gzy/algo/month202308/day20230829/GroupAnagrams.java
@@ -0,0 +1,19 @@
+package com.gzy.algo.month202308.day20230829;
+
+import java.util.List;
+
+public class GroupAnagrams {
+
+    /**
+     * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+     * An anagram is a word or phrase formed by
+     * @param strs
+     * @return
+     */
+
+    public List<List<String>> groupAnagrams(String[] strs){
+
+
+
+    }
+}
