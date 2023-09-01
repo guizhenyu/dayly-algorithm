59fc9a2aa14f6b48f886ed3222e8a2681b8c6ca0
guizhenyu <guizy@deepblueai.com>
2023-09-01 12:21:51 +0800

    Add MaximumSubArray

--- src/main/java/com/gzy/algo/month202308/day20230831/ClimbingStairs.java
+++ src/main/java/com/gzy/algo/month202308/day20230831/ClimbingStairs.java
@@ -10,24 +10,16 @@ public class ClimbingStairs {
      */
 
     public int climbStairs(int n){
-
         int[] dp = new int[n + 1];
-
         dp[1] = 1;
-
         dp[0] = 1;
         for (int i = 2; i <= n; i++){
-
             dp[i] = dp[i - 1] + dp[i - 2];
-
         }
-
-
         return dp[n];
     }
 
     public int climbStairs1(int n){
-
         int pre1 = 1;
         int pre0 = 1;
         int ans = 1;
@@ -37,8 +29,6 @@ public class ClimbingStairs {
             pre0 = pre1;
             pre1 = ans;
         }
-
-
         return ans;
     }
 
---
+++ src/main/java/com/gzy/algo/month202309/SeptemberFirstMaximumSubarray.java
@@ -0,0 +1,50 @@
+package com.gzy.algo.month202309;
+
+public class SeptemberFirstMaximumSubarray {
+
+    /**
+     * Given an integer array nums, find the subarray with the largest sum, and return its sum.
+     * <p>
+     * Constraints:
+     * 1 <= nums.length <= 10^5
+     * -10^4 <= nums[i] <= 10^4
+     * <p>
+     * Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach,
+     * which is more subtle.
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int ans = Integer.MIN_VALUE;
+        int[] sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i -1] + nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++){
+            ans = Math.max(ans, sums[i]);
+
+            for (int j = i + 1; j > nums.length; j++){
+                ans = Math.max(ans, sums[j] - sums[i]);
+            }
+        }
+
+        return ans;
+    }
+
+
+    public int dp(int[] nums) {
+        int ans = nums[0];
+        int preAns = 0;
+
+        for (int num : nums){
+
+            preAns = Math.max(preAns + num, num);
+            ans = Math.max(ans, preAns);
+        }
+
+        return ans;
+    }
+}
