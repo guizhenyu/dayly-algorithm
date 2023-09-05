468cd1a824e0130bf5f3f2a3b3b47714d6e765a8
guizhenyu <guizy@deepblueai.com>
2023-09-05 18:23:01 +0800

    Add September4IntersectionOfTwoLinkedList

---
+++ src/main/java/com/gzy/algo/month202309/September4IntersectionOfTwoLinkedList.java
@@ -0,0 +1,21 @@
+package com.gzy.algo.month202309;
+
+public class September4IntersectionOfTwoLinkedList {
+
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
+
+
