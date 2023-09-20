86356c406798e75aed250b83e6676bf1a64ce3b5
guizhenyu <guizy@deepblueai.com>
2023-09-20 18:13:02 +0800

    Add September20LongestIncreasingSubsequence

---
+++ src/main/java/com/gzy/algo/month202309/September20LongestIncreasingSubsequence.java
@@ -0,0 +1,38 @@
+package com.gzy.algo.month202309;
+
+public class September20LongestIncreasingSubsequence {
+
+
+    /**
+     * Given an integer array nums, return the length of the longest strictly
+     * increasing subsequence.
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums){
+        return process(nums, 1, 0,1);
+    }
+
+    public int process(int[] nums, int index, int preIndex, int len){
+        if (index == nums.length){
+            return len;
+        }
+
+        int ans = process(nums, index + 1, preIndex, len);
+        if (nums[index] > nums[preIndex]){
+            ans = Math.max(ans,process(nums, index + 1, index, ++len));
+        }else {
+            ans = Math.max(ans, process(nums, index + 1, index, 1));
+        }
+
+        return ans;
+    }
+
+    public int dp(int[] nums){
+        int N = nums.length;
+
+
+    }
+}
