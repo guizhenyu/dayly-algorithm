eeff1025c84f182f8b7fc0dd0964e4280fa67ce0
guizhenyu <guizy@deepblueai.com>
2023-09-24 23:14:03 +0800

    Add September24ReverseLinkedList

--- src/main/java/com/gzy/algo/month202309/September22UniquePaths.java
+++ src/main/java/com/gzy/algo/month202309/September22UniquePaths.java
@@ -43,11 +43,14 @@ public class September22UniquePaths {
 
         for (int i = m - 1; i >= 0; i--){
             for (int j = n - 1; j >= 0; j--){
-                dp[i][j] += dp[i + 1][j] + dp[i][j + 1];
+                dp[i][j] +
 
-            }
 
-        }
+
+
+                        = dp[i + 1][j] + dp[i][j + 1];
+
+            }}
 
         return dp[0][0];
     }
---
+++ src/main/java/com/gzy/algo/month202309/September24ReverseLinkedList.java
@@ -0,0 +1,41 @@
+package com.gzy.algo.month202309;
+
+public class September24ReverseLinkedList {
+
+    /**
+     * Given the head of a singly linked list,
+     * reverse the list, and return the reversed list.
+     *
+     *
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+
+
+        return process(head, null);
+    }
+
+    private ListNode process(ListNode node, ListNode pre) {
+
+        if (node == null){
+            return pre;
+        }
+
+        ListNode next = node.next;
+
+        node.next = pre;
+
+        return process(next, node);
+    }
+
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
