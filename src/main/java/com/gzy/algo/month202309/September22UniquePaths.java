ecb31e20bf41c813b17c5bd1fb00269f15842938
guizhenyu <guizy@deepblueai.com>
2023-09-24 23:16:57 +0800

    Add September22UniquePaths

--- src/main/java/com/gzy/algo/month202309/September22UniquePaths.java
+++ src/main/java/com/gzy/algo/month202309/September22UniquePaths.java
@@ -43,12 +43,7 @@ public class September22UniquePaths {
 
         for (int i = m - 1; i >= 0; i--){
             for (int j = n - 1; j >= 0; j--){
-                dp[i][j] +
-
-
-
-
-                        = dp[i + 1][j] + dp[i][j + 1];
+                dp[i][j] += dp[i + 1][j] + dp[i][j + 1];
 
             }}
 
