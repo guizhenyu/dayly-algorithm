091ec614c9c6b3d5acc698c8cc45ae3a00995c82
guizhenyu <guizy@deepblueai.com>
2023-10-19 11:04:12 +0800

    Add October18

---
+++ src/main/java/com/gzy/algo/month202310/OctoberTrappingRainWater.java
@@ -0,0 +1,41 @@
+package com.gzy.algo.month202310;
+
+import java.util.Stack;
+
+public class OctoberTrappingRainWater {
+
+    /**
+     * Given an non-negative integers representing an elevation map where the width of each bar is 1,
+     * compute how much water it can trap after raining.
+     *
+     *
+     *
+     * @param height
+     * @return
+     */
+    public int trap(int[] height){
+
+        if (height == null || height.length <= 2){
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<Integer>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i++){
+            while (!stack.isEmpty() && height[stack.peek()] <= height[i]){
+                Integer index = stack.pop();
+                if (height[index] == height[i]){
+                    break;
+                }
+                if (!stack.isEmpty()){
+                    int h = Math.min(height[i], height[stack.peek()]);
+                    ans += (i - stack.peek() - 1) * (h - height[index]);
+                }
+            }
+
+            stack.push(i);
+
+        }
+        return ans;
+    }
+
+}
