24c81ee5a6ceaac012955150552a24a367649794
guizhenyu <guizy@deepblueai.com>
2023-11-21 17:40:50 +0800

    update November21CanIWin

--- src/main/java/com/gzy/algo/month202311/November21CanIWin.java
+++ src/main/java/com/gzy/algo/month202311/November21CanIWin.java
@@ -4,36 +4,83 @@ public class November21CanIWin {
 
     public boolean canIWin(int maxChoosableInteger, int desireTotal){
 
+        if (maxChoosableInteger >= desireTotal){
+            return true;
+        }
+
+        if ((maxChoosableInteger * (maxChoosableInteger + 1) >> 1) < desireTotal){
+            return false;
+        }
+
+        int[] nums = new int[maxChoosableInteger];
+
+        for (int i = 0; i < maxChoosableInteger; i++){
+            nums[i] = i + 1;
+        }
+        return process(nums, desireTotal);
     }
 
-    public boolean process(int maxInteger, int left){
+    public boolean process(int[] nums, int rest){
+        if (rest <= 0){
+            return false;
+        }
+        for (int i = 0; i < nums.length; i++){
 
-        if (maxInteger >= left){
+            if (nums[i] != -1){
+                int cur = nums[i];
+                nums[i] = -1;
+                boolean next = process(nums, rest - cur);
+                nums[i] = cur;
+                if (!next){
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+
+    public boolean canIWinWithDP(int maxChoosableInteger, int desireTotal){
+        if (maxChoosableInteger >= desireTotal){
             return true;
+        }
+
+        if ((maxChoosableInteger * (maxChoosableInteger + 1) >> 1) < desireTotal){
+            return false;
+        }
+
+        int[] dp = new int[1 << maxChoosableInteger];
+
+        return processWithDP(maxChoosableInteger, 0, desireTotal, dp);
+    }
+
+    private boolean processWithDP(int maxNum, int status, int rest, int[] dp) {
+
+        if (dp[status] != 0){
+            return dp[status] == 1? true : false;
+        }
+
+        boolean ans = false;
+        if (rest > 0){
+            for (int i = 1; i <= maxNum; i++){
+
+                if (((1 << i) & status) == 0){
+                    if (!processWithDP(maxNum, status | (1 << i), rest - i, dp)){
+                        ans = true;
+                        break;
+                    }
+                }
+
+            }
         }
 
 
 
-
-
+        dp[status] = ans? 1 : -1;
+        return ans;
     }
 
 
-    public boolean first(int maxInteger, int left){
-        if (maxInteger >= left){
-            return true;
-        }
-
-
-
-
-    }
-
-    public boolean second(int maxInteger, int left){
-        if (maxInteger >= left){
-            return true;
-        }
-
-
-    }
 }
