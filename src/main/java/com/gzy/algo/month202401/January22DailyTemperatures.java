2325afcff8143979602f4ad7dbf3b41df9dcc9a8
guizhenyu <guizy@deepblueai.com>
2024-01-22 17:50:02 +0800

    2024 January22DailyTemperatures

---
+++ src/main/java/com/gzy/algo/month202401/January22DailyTemperatures.java
@@ -0,0 +1,34 @@
+package com.gzy.algo.month202401;
+
+import java.util.Stack;
+
+public class January22DailyTemperatures {
+
+
+    /**
+     * Given an array of integers temperatures represents the daily temperatures,
+     * return an array answer such that answer[i] is
+     *
+     * @param temperatures
+     * @return
+     */
+    public int[] dailyTemperatures(int[] temperatures){
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(0);
+
+        int[] ans = new int[temperatures.length];
+        for (int i = 1; i < temperatures.length; i++){
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                Integer pop = stack.pop();
+                ans[pop] = i - pop;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()){
+            ans[stack.pop()] = 0;
+        }
+
+        return ans;
+    }
+}
