c200c01033459d1214bba45b7ac565196b0f05b0
guizhenyu <guizy@deepblueai.com>
2024-03-05 17:32:11 +0800

    LowestCommonAncestorOfABinaryTree

--- src/main/java/com/gzy/algo/month202403/PalindromePartitioning.java
+++ src/main/java/com/gzy/algo/month202403/PalindromePartitioning.java
@@ -1,5 +1,6 @@
 package com.gzy.algo.month202403;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,22 +13,49 @@ public class PalindromePartitioning {
      * @return
      */
 
-
+    public boolean[][] dp;
+    public int n;
+    List<List<String>> ret = new ArrayList<>();
+    List<String> ans = new ArrayList<>();
 
     public List<List<String>> partition(String s){
+        n = s.length();
 
-    }
-
-    public static void main(String[] args) {
-
-        boolean[][] dp = new boolean[5][5];
-        for (int i = 0; i < 5; i++){
+        dp = new boolean[n][n];
+        for (int i = 0; i < n; i++){
             Arrays.fill(dp[i], true);
         }
 
+        for (int l = n - 1; l>= 0; l--){
+            for (int r = l + 1; r < n; r++){
 
-        System.out.println(dp);
+                dp[l][r] = s.charAt(l) == s.charAt(r);
+                if(l + 1 <= r - 1){
+                    dp[l][r] =  dp[l][r] && dp[l + 1][r - 1];
+                }
 
+            }
+        }
+
+        dfs(s, 0);
+
+        return ret;
+
+    }
+
+    private void dfs(String s, int i) {
+
+        if (i == n){
+            ret.add(new ArrayList<>(ans));
+        }
+
+        for (int j = i; j < n; j++){
+            if (dp[i][j]){
+                ans.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
     }
 
 
